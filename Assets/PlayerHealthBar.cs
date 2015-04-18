4916b741eea5d3231df7e1c1efcf6a97b54703b3
Dallon Feldner <dallonfdev@gmail.com>
2015-04-18 13:29:52 -0500

    Added player health bar

---
+++ Assets/PlayerHealthBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public PlayerHealth PlayerHealth;
+    public Slider Slider;
+
+    public void Awake()
+    {
+        PlayerHealth = FindObjectOfType<PlayerHealth>();
+        Slider = GetComponent<Slider>();
+    }
+
+    public void FixedUpdate()
+    {
+        if (PlayerHealth)
+        {
+            Slider.value = (float)PlayerHealth.Health / PlayerHealth.MaxHealth;
+        }
+        else
+        {
+            Slider.value = 0;
+        }
+    }
+}

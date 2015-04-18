8ea5816207334aff913d91013ec289bcadc47c9e
Dallon Feldner <dallonfdev@gmail.com>
2015-04-18 18:03:03 -0500

    Added Enemies Destroyed count

---
+++ Assets/EnemiesDestroyedText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemiesDestroyedText : MonoBehaviour
+{
+    private Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        text.text = "Enemies Destroyed: " + ScoreManager.Instance.EnemiesDestroyed;
+    }
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+    public int EnemiesDestroyed = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        Instance = this;
+    }
+}

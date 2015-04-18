9be6f80161dd211224294e998c8e93f20d98beee
Dallon Feldner <dallonfdev@gmail.com>
2015-04-18 11:53:50 -0500

    Added logic to MetaGun

---
+++ Assets/MetaGunController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MetaGunController : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public float ShotsToFire = 3;
+    public float DelayToFire = 0.5f;
+    public float DelayBetweenShots = 0.25f;
+    public float TimeAfterFire = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(FireWeapon());
+    }
+
+    IEnumerator FireWeapon()
+    {
+        yield return new WaitForSeconds(DelayToFire);
+        for (int i = 0; i < ShotsToFire; i++)
+        {
+            Debug.Log("Shoot");
+            yield return new WaitForSeconds(DelayBetweenShots);
+        }
+        yield return new WaitForSeconds(TimeAfterFire);
+        Destroy(gameObject);
+    }
+}

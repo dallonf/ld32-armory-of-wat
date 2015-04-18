34e229c82ceafcfda58ce7fa1f3492265dafe1dd
Dallon Feldner <dallonfdev@gmail.com>
2015-04-18 09:16:48 -0500

    Hatman now aims at the mouse cursor

---
+++ Assets/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Extensions
+{
+    public static float GetLookAtAngle2D(this Transform transform, Vector3 target)
+    {
+        var diff = target - transform.position;
+        float rotZ = AngleMath.VectorToAngle2D(diff);
+        return rotZ;
+    }
+}
+
+public static class AngleMath
+{
+    public static float VectorToAngle2D(Vector3 vector)
+    {
+        return Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+    }
+}
--- Assets/PlayerAim.cs
+++ Assets/PlayerAim.cs
@@ -3,16 +3,42 @@ using System.Collections;
 
 public class PlayerAim : MonoBehaviour
 {
+    public float MinTargetDistance = 1;
+    public Transform GunPointer;
 
-    // Use this for initialization
-    void Start()
+    private float CurrentAngle = 0;
+
+    void Awake()
     {
-
+        GunPointer = transform.FindChild("gun-pointer");
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Vector2 mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.DrawLine(transform.position, mousePositionInWorld, Color.blue);
 
+        var diff = mousePositionInWorld - (Vector2)GunPointer.position;
+        if (diff.sqrMagnitude < MinTargetDistance * MinTargetDistance)
+        {
+            diff = mousePositionInWorld - (Vector2)transform.position;
+        }
+
+        var angle = AngleMath.VectorToAngle2D(diff);
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (GunPointer)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(GunPointer.position, GunPointer.TransformDirection(Vector3.up) * 100);
+        }
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, MinTargetDistance);
     }
 }

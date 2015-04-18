d154df6989e210c3555b8735ee87ea01089a4bcc
Dallon Feldner <dallonfdev@gmail.com>
2015-04-18 12:06:10 -0500

    METAGUN!!

---
+++ Assets/BulletController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float Speed = 30f;
+
+    private Rigidbody2D rigidbody2d;
+
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidbody2d.MovePosition(transform.position + transform.right * Speed * Time.deltaTime);
+    }
+}

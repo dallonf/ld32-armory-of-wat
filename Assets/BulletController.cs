87925dd20b753dc9d80b512dd09bebb6766ba514
Dallon Feldner <dallonfdev@gmail.com>
2015-04-18 12:07:06 -0500

    Bullets clean themselves up after time

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -5,12 +5,14 @@ public class BulletController : MonoBehaviour
 {
 
     public float Speed = 30f;
+    public float TTL = 2;
 
     private Rigidbody2D rigidbody2d;
 
     void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, TTL);
     }
 
     // Update is called once per frame

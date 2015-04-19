0d8d4104cc5ac8ef875d7994ee48077daf226901
Dallon Feldner <dallonfdev@gmail.com>
2015-04-19 15:44:28 -0500

    T-Gun no longer uses coroutine - this is important for enabling/disabling it

--- Assets/TGunController.cs
+++ Assets/TGunController.cs
@@ -13,7 +13,7 @@ public class TGunController : MonoBehaviour
     private Collider2D playerCollider;
     private AudioSource audioSource;
 
-    private bool IsCoolingDown;
+    private float cooldownRemaining = 0;
 
     public void Awake()
     {
@@ -24,20 +24,18 @@ public class TGunController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !IsCoolingDown)
+        if (Input.GetMouseButtonDown(0) && cooldownRemaining <= 0)
         {
             Transform launchPoint = Random.value > 0.5f ? LaunchPoint1 : LaunchPoint2;
             var bullet = (GameObject)GameObject.Instantiate(BulletPrefab, launchPoint.position, launchPoint.rotation);
             Physics2D.IgnoreCollision(playerCollider, bullet.GetComponent<Collider2D>());
             audioSource.PlayOneShot(ShootSound);
-            StartCoroutine(Cooldown());
+            cooldownRemaining = CooldownTime;
         }
-    }
 
-    private IEnumerator Cooldown()
-    {
-        IsCoolingDown = true;
-        yield return new WaitForSeconds(CooldownTime);
-        IsCoolingDown = false;
+        if (cooldownRemaining > 0)
+        {
+            cooldownRemaining -= Time.deltaTime;
+        }
     }
 }

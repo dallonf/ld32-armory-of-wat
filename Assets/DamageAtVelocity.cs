983a7cb7e9698805c79a46c4010cddb8c15efc5f
Dallon Feldner <dallonfdev@gmail.com>
2015-04-19 16:37:24 -0500

    Added ROCKET GUN and it's amazing :D

---
+++ Assets/DamageAtVelocity.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageAtVelocity : MonoBehaviour
+{
+    public ParticleSystem ParticleSystemWhenDamaging;
+    public float MinSpeed = 4;
+    public float MaxTime = 2;
+    public float MaxTimeAfterCollision = 0.25f;
+    public int Damage = 50;
+
+    private float timeSinceStart = 0;
+    private float timeSinceCollision = 0;
+    private bool hasCollided = false;
+
+    private bool isDamaging = false;
+
+    private Rigidbody2D rigidbody2d;
+
+    public void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDamaging)
+        {
+            timeSinceStart += Time.deltaTime;
+            //if (hasCollided)
+            //{
+            //    timeSinceCollision += Time.deltaTime;
+            //}
+
+            if (timeSinceStart > MaxTime)
+            {
+                StopDamaging();
+            }
+            //else if (hasCollided && timeSinceCollision > MaxTimeAfterCollision)
+            //{
+            //    Debug.Log("Collision timeout");
+            //    StopDamaging();
+            //}
+            else if (Vector2.SqrMagnitude(rigidbody2d.velocity) < MinSpeed*MinSpeed)
+            {
+                StopDamaging();
+            }
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isDamaging)
+        {
+            collision.gameObject.SendMessage("TakeDamage", Damage, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    public void StopDamaging()
+    {
+        isDamaging = false;
+        ParticleSystemWhenDamaging.enableEmission = false;
+    }
+
+    public void StartDamaging()
+    {
+        isDamaging = true;
+        ParticleSystemWhenDamaging.enableEmission = true;
+        timeSinceStart = 0;
+        timeSinceCollision = 0;
+        hasCollided = false;
+    }
+}
---
+++ Assets/RocketGun.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketGun : MonoBehaviour
+{
+
+    public float Force = 1;
+    public AudioClip FireSound;
+    public float MuzzleFlashTime = 0.1f;
+    public float CooldownTime = 0.4f;
+
+    private Rigidbody2D playerRigidbody;
+    private AudioSource audioSource;
+    private SpriteRenderer muzzleFlash;
+
+    private float currentMuzzleFlashTime = 0;
+    private float cooldownRemaining = 0;
+
+    public void Awake()
+    {
+        playerRigidbody = GetComponentInParent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+        muzzleFlash = transform.FindChild("muzzle-flash").GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && cooldownRemaining <= 0)
+        {
+            audioSource.PlayOneShot(FireSound);
+            playerRigidbody.AddForce(Force * transform.right * -1, ForceMode2D.Impulse);
+            muzzleFlash.enabled = true;
+            currentMuzzleFlashTime = MuzzleFlashTime;
+            cooldownRemaining = CooldownTime;
+            SendMessageUpwards("StartDamaging");
+        }
+
+        if (cooldownRemaining > 0)
+        {
+            cooldownRemaining -= Time.deltaTime;
+        }
+
+        if (currentMuzzleFlashTime > 0)
+        {
+            currentMuzzleFlashTime -= Time.deltaTime;
+            if (currentMuzzleFlashTime <= 0)
+            {
+                muzzleFlash.enabled = false;
+            }
+        }
+    }
+}
